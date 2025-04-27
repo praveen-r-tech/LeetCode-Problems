7d7a023989ec3f62f095119b9b81a2e9f66a142e
Praveen R <rajupraveen.2005@gmail.com>
2025-04-27 21:51:02 +0530

    Create Solution.java

---
+++ 2011. Final Value of Variable After Performing Operations/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+       int x=0;
+       for(String s: operations){
+        if(s.equals("--X") || s.equals("X--")){
+            x-=1;
+        }else{
+            x+=1;
+        }
+       } 
+       return x;
+    }
+}
