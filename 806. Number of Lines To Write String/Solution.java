11216694518a939da94c3df840b22fbc1bcb05b5
Praveen R <rajupraveen.2005@gmail.com>
2025-05-09 14:15:47 +0530

    Create Solution.java

---
+++ 806. Number of Lines To Write String/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] numberOfLines(int[] widths, String s) {
+      int lines= 1, width= 0;
+      for(char c: s.toCharArray()){
+          int w= widths[c - 'a'];
+          width += w;
+          if(width>100) {
+            lines++;
+            width= w;
+          }
+      }
+      return new int[]{lines, width};
+    }
+}
