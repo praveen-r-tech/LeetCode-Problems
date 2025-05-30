0c116a5a7d1d10d757d931edc6433b2a215852cd
Praveen R <rajupraveen.2005@gmail.com>
2025-05-30 09:38:25 +0530

    Create Solution.java

---
+++ 1957. Delete Characters to Make Fancy String/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String makeFancyString(String s) {
+      StringBuilder str= new StringBuilder(s);
+      for(int i=1; i<str.length()-1; i++){
+        if(str.charAt(i-1)==str.charAt(i) && str.charAt(i)==str.charAt(i+1)){
+            str.deleteCharAt(i);
+            i--;
+        }
+      }
+      return str.toString();
+    }
+}
