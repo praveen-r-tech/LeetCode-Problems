6c203c8c7d7c53e1c8341ae6125cfa462f990495
Praveen R <rajupraveen.2005@gmail.com>
2025-09-25 16:34:45 +0530

    Update Solution.java

--- 3005. Count Elements With Maximum Frequency/Solution.java
+++ 3005. Count Elements With Maximum Frequency/Solution.java
@@ -1,14 +1,18 @@
 class Solution {
-    public int[] constructRectangle(int area) {
-        int w=(int) Math.sqrt(area);
-        while(w>0){
-            if(area%w==0){
-                int l=area/w;
-                return new int[]{l,w};
-            }
-            w--;
+    public int maxFrequencyElements(int[] nums) {
+        int max=0;
+        int[] freq=new int[101];
+        for(int i=0; i<nums.length; i++){
+            int idx= nums[i];
+            freq[idx]++;
+            if(freq[idx]>max) max=freq[idx];
         }
-        
-        return new int[]{area,1}; 
-    }
+        int total=0;
+        for(int i=0; i<101; i++){
+            if(freq[i]==max){
+                total+=freq[i];
+            }
+        }
+        return total;
+    }
 }
