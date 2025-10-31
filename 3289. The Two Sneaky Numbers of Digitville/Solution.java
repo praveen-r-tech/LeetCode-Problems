09f60ba573d8c50f8d55eea8d40f508368277ab0
Praveen R <rajupraveen.2005@gmail.com>
2025-10-31 12:50:20 +0530

    Create Solution.java

---
+++ 3289. The Two Sneaky Numbers of Digitville/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] getSneakyNumbers(int[] nums) {
+       Set<Integer> set=new HashSet<>();
+       int[] ans=new int[2];
+       int idx=0;
+       for(int num: nums){
+          if(set.contains(num)) ans[idx++]=num;
+          else set.add(num);
+       }
+       return ans;
+    }
+}
