d95d351527f3b4ed36bf55642da977a77008b25f
Praveen R <rajupraveen.2005@gmail.com>
2025-11-13 14:22:02 +0530

    Create Solution.java

---
+++ 1827. Minimum Operations to Make the Array Increasing/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minOperations(int[] nums) {
+       if(nums.length==1) return 0;
+       int total=0;
+       for(int i=1; i<nums.length; i++){
+           if(nums[i]<nums[i-1]){
+              int val=nums[i-1]-nums[i]+1;
+              total+=val;
+              nums[i]+=val;
+           }else if(nums[i]==nums[i-1]){
+              total++;
+              nums[i]++;
+           }
+       }
+       return total; 
+    }
+}
