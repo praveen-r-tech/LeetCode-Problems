eafa8ae6fb76bc09aae9c761a330434eef339fe6
Praveen R <rajupraveen.2005@gmail.com>
2025-11-24 16:07:31 +0530

    Create Solution.java

---
+++ 1816. Truncate Sentence/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String truncateSentence(String s, int k) {
+       String[] word=s.split(" ");
+       if(word.length==k) return s;
+       StringBuilder sb=new StringBuilder();
+       for(int i=0; i<k; i++){
+           sb.append(word[i]);
+           if(i!=k-1) sb.append(" ");
+       } 
+       return sb.toString();
+    }
+}
