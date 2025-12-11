7a69aed73c244499a1bf5aba8b613ca7450f1bce
Praveen R <rajupraveen.2005@gmail.com>
2025-12-11 16:53:46 +0530

    Create Solution.java

---
+++ 1925. Count Square Sum Triples/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int countTriples(int n) {
+       int res=0;
+       for(int a=1; a<=n; a++){
+          for(int b=1; b<=n; b++){
+              int c=(int) Math.sqrt(a*a+b*b);
+              if (c<=n && c*c==a*a+b*b) res++;
+          }
+       }
+       return res; 
+    }
+}
