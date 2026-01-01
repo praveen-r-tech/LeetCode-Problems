396de5ebb157282fddb57f8113a16d4a83829bb7
Praveen R <rajupraveen.2005@gmail.com>
2026-01-01 22:27:17 +0530

    Create Solution.java

---
+++ 2716. Minimize String Length/Solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int minimizedStringLength(String s) {
+       Set<Character> set=new HashSet<>();   
+       for(char c: s.toCharArray()){
+           if(!set.contains(c)) set.add(c);
+       }
+       return set.size();
+    }
+}
