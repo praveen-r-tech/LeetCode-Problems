76aab1d1e1422ac52a9f53d9f4831713707068da
Praveen R <rajupraveen.2005@gmail.com>
2026-01-04 18:35:10 +0530

    Create Solution.java

---
+++ 3658. GCD of Odd and Even Sums/Solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    int gcd(int a,int b){
+        if(b==0) return a;
+        return gcd(b, a%b);
+    }
+    public int gcdOfOddEvenSums(int n) {
+        return gcd(n*n, n*(n+1));
+    }
+}
