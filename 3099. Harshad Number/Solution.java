1cfd17aed576868d478b488f15c6381e897f0dff
Praveen R <rajupraveen.2005@gmail.com>
2026-01-07 19:49:26 +0530

    Create Solution.java

---
+++ 3099. Harshad Number/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int sumOfTheDigitsOfHarshadNumber(int x) {
+      int sum=0;
+      int tmp=x;
+      while(tmp>0){
+        sum+=tmp%10;
+        tmp/=10;
+      } 
+      if(x%sum==0) return sum;
+      return -1; 
+    }
+}
