6ddeae705a3953b2d7336a990b2173c008028a0c
Praveen R <rajupraveen.2005@gmail.com>
2026-03-04 08:33:42 +0530

    Create Solution.java

---
+++ 29. Divide Two Integers/Solution.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int divide(int dividend, int divisor) {
+        long q=dividend/divisor;
+        if(dividend<=-2147483648 && divisor==1) return -2147483648;
+        if(dividend<=-2147483648 && divisor==-1) return 2147483647;
+        return (int)q;
+    }
+}
